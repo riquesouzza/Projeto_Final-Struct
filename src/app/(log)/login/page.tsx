32bc0ec3bf5a0ca2c922c993d9cdf8a141a39d24
Henri <henriquesouzzanunes@gmail.com>
2025-12-14 21:31:05 -0300

    registrar

--- src/app/(log)/login/page.tsx
+++ src/app/(log)/login/page.tsx
@@ -14,14 +14,14 @@ export default function Entrar() {
     <section className="min-h-screen bg-[var(--steam-mid)] flex items-center justify-center p-6">
       <div className="w-full max-w-md bg-[var(--steam-dark)] p-8 rounded-2xl shadow-xl">
         <h1 className="text-2xl font-bold text-white text-center mb-6">
-          Entrar
+          Entrar/Registrar
         </h1>
         
         <button 
           onClick={() => signIn("google", { callbackUrl: "/usuario" })}
           className="w-full py-3 bg-white text-gray-800 rounded-lg hover:bg-gray-100 transition font-medium"
         >
-          Entrar com Google
+          Entrar/Registrar com Google
         </button>
       </div>
     </section>
